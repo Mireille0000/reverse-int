5f4b662171f91a6cd8e0d9b5d0b0ec97468b79a2
Gasanova Tamara <solas93@mail.ru>
2023-02-26 16:13:39 +0300

    fix: optimise the function

--- src/index.js
+++ src/index.js
@@ -1,8 +1,3 @@
 module.exports = function reverse (n) {
-  let numberR = Math.sign(n)*parseInt(n.toString().split('').reverse().join(''));
-
-  if (numberR > 0) {
-    return numberR;
-  } 
-  return numberR * -1;
+  return parseInt(n.toString().split('').reverse().join(''));
 }
