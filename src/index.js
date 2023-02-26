6ead553e57b9b99ff9f8e18c95085357ac23f728
Gasanova Tamara <solas93@mail.ru>
2023-02-26 16:00:52 +0300

    docs: reverseInt function task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 module.exports = function reverse (n) {
-  
+  let numberR = Math.sign(n)*parseInt(n.toString().split('').reverse().join(''));
+
+  if (numberR > 0) {
+    return numberR;
+  } 
+  return numberR * -1;
 }
